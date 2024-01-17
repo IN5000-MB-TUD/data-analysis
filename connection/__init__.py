c66d3c8b29ca236328ca32ddcb77fbfcfb28af62
Mattia Bonfanti <bonfantimattia@gmail.com>
2024-01-17 16:47:55 +0100

    Add GitHub data mining
    - Add GitHub API data mining

--- connection/__init__.py
+++ connection/__init__.py
@@ -3,6 +3,9 @@ import logging
 from connection import mongo
 
 # Setup logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s : %(message)s"
+)
 log = logging.getLogger(__name__)
 
 log.info("Connecting to MongoDB...")
