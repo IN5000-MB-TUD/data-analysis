229c8d4e3397c0d7dac371cde20b1f9afd10dcd2
Mattia Bonfanti <bonfantimattia@gmail.com>
2024-01-19 14:28:14 +0100

    Add issues and stars stats
    - Add issues and stars stats

--- data_mining/statistics.py
+++ data_mining/statistics.py
@@ -20,20 +20,65 @@ if __name__ == "__main__":
     repositories = mo.db["repositories_data"].find()
 
     for repository in repositories:
-        repository_statistics = github_api_client.get_weekly_commits_statistics(repository["owner"], repository["name"])
+        update_flag = False
 
-        # If new statistics are available, update the object
         # The api does not process repos with more than 10000 commits, so those are skipped
-        if repository_statistics and repository["commits"] <= 10000:
+        if repository["commits"] <= 10000:
+            repository_statistics = github_api_client.get_weekly_commits_statistics(repository["owner"], repository["name"])
+
+            # If new statistics are available, update the object
+            if repository_statistics:
+                mo.db["repositories_data"].update_one(
+                    {"full_name": repository["full_name"]},
+                    {"$set": {
+                        "statistics": {
+                            "commits_weekly": repository_statistics,
+                        }
+                    }}
+                )
+                log.info("Successfully updated commits data for {}".format(repository["full_name"]))
+                update_flag = True
+
+        # Gather time series about stargazers and issues
+        # The operations are executed if the db entry has been updated more than 1 day ago
+        if (datetime.now(tz=utc) - repository["metadata"]["modified"]).days < 1:
+            log.info("Skipping repository {} since it was updated less than 1 day ago.".format(repository["full_name"]))
+            continue
+
+        repository_stargazers = github_api_client.get_repository_stargazers_time(repository["owner"], repository["name"])
+        if repository_stargazers:
             mo.db["repositories_data"].update_one(
                 {"full_name": repository["full_name"]},
                 {"$set": {
                     "statistics": {
-                        "commits_weekly": repository_statistics,
-                    },
+                        "stargazers": repository_stargazers,
+                    }
+                }}
+            )
+            log.info("Successfully updated stargazers time series for {}".format(repository["full_name"]))
+            update_flag = True
+
+        repository_issues = github_api_client.get_repository_issues_time(repository["owner"], repository["name"])
+        if repository_issues:
+            mo.db["repositories_data"].update_one(
+                {"full_name": repository["full_name"]},
+                {"$set": {
+                    "statistics": {
+                        "issues": repository_issues,
+                    }
+                }}
+            )
+            log.info("Successfully updated issues time series for {}".format(repository["full_name"]))
+            update_flag = True
+
+        # Update the metadata
+        if update_flag:
+            mo.db["repositories_data"].update_one(
+                {"full_name": repository["full_name"]},
+                {"$set": {
                     "metadata": {
                         "modified": datetime.now(tz=utc),
                     }
                 }}
             )
-            log.info("Successfully updated statistics for {}".format(repository["full_name"]))
+            log.info("Successfully updated statistics data for {} -------------".format(repository["full_name"]))
